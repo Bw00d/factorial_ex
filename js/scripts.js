c520254d59f9d1b72cb990c1421f0748e3a84717
Brentwood Prescott Reid & Lind <student@epicodus.com>
2014-02-20 11:31:04 -0800

    make output array of products

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,61 @@
+var productsArray;
+
 var factorial = function(input) {
 
   if (!input.isNaN && input >= 0) {
 
-    var numbers = [];
     var result = 1;
-    for (var i = input; i > 0; i--) {
-      numbers.push(i);
-      result *= i;
+    var products = [];
+
+    if (input === 0) {
+      result = 1;
+    } else {
+      for (var i = input; i > 1; i--) {
+
+        result *= i;
+        products.push(result);
+
+        //outputResults(result);
+      }
     }
+
+    productsArray = products;
 
     return result;
   } else {
     return false;
   }
 };
+
+
+$(document).ready(function() {
+
+  var adjustProducts = function(myProducts) {
+
+    newProductsArray = myProducts.split[','];
+
+    for (var x = 0; x < newProductsArray.length; x++) {
+      $('#triangle').append(newProductsArray[x] + ' - ');
+    }
+
+  };
+
+  $('#factorial').submit(function(event) {
+
+
+
+
+    //$('#result').text(factorial($('#number').val()));
+    $('#result').prepend(factorial($('#number').val()));
+
+    adjustProducts(productsArray);
+
+    $('#number').val('');
+    $('#result').show();
+
+    event.preventDefault();
+  });
+
+});
+
 
