edd4aeee895caeb15e3c64b32b9cf475315b6e38
Brentwood Prescott Reid & Lind <student@epicodus.com>
2014-02-20 15:03:05 -0800

    finish recursion

--- js/recursive-script.js
+++ js/recursive-script.js
@@ -1,10 +1,24 @@
 var recursiveFactorial = function(number) {
-  if (number > 0) {
-    console.log(number);
-    var newNumber = recursiveFactorial(number - 1);
-    return newNumber;
+  if (parseInt(number) === 0) {
+    return 1;
+  } else if (!number.isNaN && number > 0) {
+    return number * recursiveFactorial(number - 1);
   } else {
-    return number;
+    return false;
   }
-
 };
+
+$(document).ready(function() {
+
+  $('#factorial').submit(function(event) {
+    $('#final-result').empty();
+    var myNumber = $('#number').val();
+    $('#final-result').prepend(recursiveFactorial(myNumber));
+
+    $('#number').val('');
+    $('#result').show();
+
+    event.preventDefault();
+  });
+
+});
--- spec/recursive-specs.js
+++ spec/recursive-specs.js
@@ -1,15 +1,18 @@
 describe('recursiveFactorial', function(){
-  /*it("is false if the inputted value is not a number", function(){
-     factorial("t").should.equal(false);
-  });
+  // it("is false if the inputted value is not a number", function(){
+  //    recursiveFactorial("t").should.equal(false);
+  // });
 
-  it("is false if the inputted value is not a negative number", function() {
-    factorial(-1).should.equal(false);
-  });*/
+  // it("is false if the inputted value is not a negative number", function() {
+  //   recursiveFactorial(-1).should.equal(false);
+  // });
 
-  it("will decrement itself, until it reaches 0", function() {
-    recursiveFactorial(9).should.equal(0);
-  });
+  // it("will decrement itself, until it reaches 0", function() {
+  //   recursiveFactorial(9).should.equal(0);
+  // });
+  it("will multiply each decremental number by itself", function() {
+    recursiveFactorial(5).should.equal(120);
+  })
 
   /*
 
